d066df34fe49aa964a5106de2f0b5ee600d0cb54
Austin Robertson <145944031+arob191@users.noreply.github.com>
2024-02-29 16:54:12 -0600

    Debugging complete

--- Beginner/Debuggin.py
+++ Beginner/Debuggin.py
@@ -36,11 +36,44 @@
 # print(word_per_page)
 
 # #Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-    b_list.append(new_item)
-  print(b_list)
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#     b_list.append(new_item)
+#   print(b_list)
 
-mutate([1,2,3,5,8,13])
+# mutate([1,2,3,5,8,13])
+
+# number = int(input("Which number do you want to check?")) # Which number do you want to check?
+
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# Which year do you want to check?
+
+# year = int(input("What year do you want to check? "))
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+# target = int(input("What number do you want to check? "))
+# for number in range(1, target - 1):
+#   if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
