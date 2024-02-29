8c3625975b76b3d6712af5891b2e7725d6613cce
Austin Robertson <145944031+arob191@users.noreply.github.com>
2024-02-29 17:32:27 -0600

    Started_higher_lower

--- Beginner/higher_or_lower.py
+++ Beginner/higher_or_lower.py
@@ -15,14 +15,24 @@ def set_person():
 person1 = set_person()
 person2 = set_person()
 
-print(f"Compare A: {person1['name']}, a {person1['description']}, from {person1['country']}")
-
 # Next we need to provide a comparison line the shows the first random person vs the second random person
+
+print(f"Compare A: {person1['name']}, a {person1['description']}, from {person1['country']}")
+print(higher_or_lower_art.vs)
+print(f"Compare B: {person2['name']}, a {person2['description']}, from {person2['country']}")
 
 # We need to ask the user which one has more followers
 
-# We need to dermine who has more followers
+input("Who has more followers? 'A' or 'B': ").lower()
 
+# We need to dermine who has more followers
+def determine_higher(p1, p2):
+    if p1 > p2:
+        return p1
+    else:
+        return p2
+
+print(determine_higher(person1['follower_count'], person2['follower_count']))
 # We need to compare the answer of the user to the randomly selected people
 
 # If the user answer is wrong we need end the game
