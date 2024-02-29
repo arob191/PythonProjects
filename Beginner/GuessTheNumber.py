b9a7bda4d076fce629d8ac833b1cfc0334f8dd46
Austin Robertson <145944031+arob191@users.noreply.github.com>
2024-02-28 18:41:01 -0600

    Clean up Guess code

--- Beginner/GuessTheNumber.py
+++ Beginner/GuessTheNumber.py
@@ -2,6 +2,24 @@ import os
 import GuessArt
 import random
 
+def check_guess(guess, answer, lives):
+        if guess == answer:
+            print(f"You Win! The number was {answer}")
+        if guess > answer:
+            print("Guess is too high!")
+            return lives - 1
+        if guess < answer:
+            print(f"Guess is too low!")
+            return lives - 1
+
+def change_lives(user_difficulty):
+        if user_difficulty == "hard":
+            print("You chose hard! Lets begin")
+            return 5
+        if user_difficulty == "easy":
+            print("You chose easy! Lets begin")
+            return 10
+
 def game():
     
     print(GuessArt.logo)
@@ -10,47 +28,17 @@ def game():
 
     ask_continue = ("Play again? Type 'y' or 'n':")
 
-    def change_lives(User_difficulty):
-        if User_difficulty == "hard":
-            return Lives + 5
-        if User_difficulty == "easy":
-            return Lives + 10
-
-    def check_guess(guess):
-        if guess == guess_number:
-            return 1
-        if guess > guess_number:
-            return 2
-        if guess < guess_number:
-            return 3
-            
-
-    # of lives
-    Lives = 0
+    user_guess = 0
 
     difficulty = input("Welcome to the Number Guessing Game! \nI am thinking of a number between 1 and 100. \nChoose the difficulty. Type 'Easy' or 'Hard': ").lower()
 
-    Lives = change_lives(difficulty)
+    lives = change_lives(difficulty)
 
-    print(f"You chose {difficulty}. Lets begin")
+    while lives > 0 & user_guess != guess_number:
 
-    while Lives > 0:
-
+        print(f"You have {lives} attempts left")
         user_guess = int(input("Make a guess:"))
-
-        if check_guess(user_guess) == 1:
-            print(f"You Win! The number was {guess_number}")
-            if input(ask_continue) == "y":
-                game()
-                os.system("cls")
-        elif check_guess(user_guess) == 2:
-            print("Guess is too high!")
-            Lives -= 1
-            print(f"You have {Lives} attempts left")
-        else:
-            print(f"Guess is too low!")
-            Lives -= 1
-            print(f"You have {Lives} attempts left")
+        lives = check_guess(user_guess, guess_number, lives) 
     
     print("Game Over!")
     if input(ask_continue) == "y":
