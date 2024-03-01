52fd141eb5d463522e42c0ad30bd81488c013371
Austin Robertson <ausrulrob@gmail.com>
2024-03-01 16:54:33 -0600

    Refactor higher_or_lower.py game logic

--- Beginner/higher_or_lower.py
+++ Beginner/higher_or_lower.py
@@ -9,33 +9,58 @@ print(higher_or_lower_art.logo)
 
 # We need to randomly select two people and assign them person 1 and person 2
 
+
 def set_person():
     return random.choice(higher_or_lower_data.data)
 
-person1 = set_person()
-person2 = set_person()
-
-# Next we need to provide a comparison line the shows the first random person vs the second random person
-
-print(f"Compare A: {person1['name']}, a {person1['description']}, from {person1['country']}")
-print(higher_or_lower_art.vs)
-print(f"Compare B: {person2['name']}, a {person2['description']}, from {person2['country']}")
-
-# We need to ask the user which one has more followers
-
-input("Who has more followers? 'A' or 'B': ").lower()
-
-# We need to dermine who has more followers
-def determine_higher(p1, p2):
-    if p1 > p2:
-        return p1
+def set_user_geuss(guess, p1, p2):
+    if guess == "a":
+        return p1['follower_count']
     else:
-        return p2
+        return p2['follower_count']
 
-print(determine_higher(person1['follower_count'], person2['follower_count']))
-# We need to compare the answer of the user to the randomly selected people
+def determine_higher(p1, p2):
+    if p1['follower_count'] > p2['follower_count']:
+        return p1['follower_count']
+    else:
+        return p2['follower_count']
 
-# If the user answer is wrong we need end the game
+def compare_answer(higher, guess, score, lives):
+    if higher == guess:
+        return
+    else:
+        print(f"Game Over! Score: {score}")
+        return lives - 1
 
-# If the user answer is right we continue and retrieve two more random people
+def game():
+    
+    score = 0
+    lives = 1
 
+    while lives != 0:
+
+        person1 = set_person()
+        person2 = set_person()
+
+        print(f"Compare A: {person1['name']}, a {person1['description']}, from {person1['country']}")
+        print(higher_or_lower_art.vs)
+        print(f"Compare B: {person2['name']}, a {person2['description']}, from {person2['country']}")
+
+        guess = input("Who has more followers? 'A' or 'B': ").lower()
+
+        higher = determine_higher(person1, person2)
+
+        user_guess = set_user_geuss(guess, person1, person2)
+
+        lives = compare_answer(higher, user_guess, score, lives)
+
+        if lives == 1:
+            score + 1
+    
+    if input("Play again? Type 'y' or 'n': ").lower() == 'y':
+        os.system('cls')
+        game()
+    else:
+        return
+
+game()
