a8366f777c40ef96a37d8871fcbf558920d6d956
Austin Robertson <ausrulrob@gmail.com>
2024-03-03 19:27:00 -0600

    Cleaned up code

--- Beginner/higher_or_lower.py
+++ Beginner/higher_or_lower.py
@@ -13,26 +13,6 @@ print(higher_or_lower_art.logo)
 def set_person():
     return random.choice(higher_or_lower_data.data)
 
-# def set_user_geuss(guess, p1, p2):
-#     if guess == "a":
-#         return p1['follower_count']
-#     else:
-#         return p2['follower_count']
-
-# def determine_higher(p1, p2):
-#     if p1['follower_count'] > p2['follower_count']:
-#         return p1['follower_count']
-#     else:
-#         return p2['follower_count']
-
-# def compare_answer(higher, guess, score, lives):
-#     if higher == guess:
-#         print("Correct!")
-#         return 1
-#     else:
-#         print(f"Game Over! Score: {score}")
-#         return lives - 1
-
 def check_answer(p1, p2, guess):
     if p1['follower_count'] > p2['follower_count']:
         return guess == 'a'
@@ -48,13 +28,12 @@ def replace_person(p2, p1):
 def game():
 
     score = 0
-    # lives = 1
     continue_game = True
     person2 = set_person()
     
     
     while continue_game:
-    # while lives != 0:
+
 
         person1 = person2
         person2 = set_person()
@@ -68,9 +47,7 @@ def game():
         print(f"Compare B: {person2['name']}, a {person2['description']}, from {person2['country']}")
 
         guess = input("Who has more followers? 'A' or 'B': ").lower()
-        # higher = determine_higher(person1, person2)
-        # user_guess = set_user_geuss(guess, person1, person2)
-        # lives = compare_answer(higher, user_guess, score, lives)
+
         is_correct = check_answer(person1, person2, guess)
          
         if is_correct:
@@ -80,9 +57,7 @@ def game():
             continue_game = False
             print(f"Wrong! Game Over. Score: {score}")
         
-        # if lives == 1:
-        #     score += 1
-    
+
     if input("Play again? Type 'y' or 'n': ").lower() == 'y':
         os.system('cls')
         game()
