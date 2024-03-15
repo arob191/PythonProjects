b6e58642967ad3ddf4e24af7a30f68027a6948b9
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-15 17:08:44 -0500

    used the pendown and penup methods to draw a dotted line

--- Intermediate/TurtleChallenge/main.py
+++ Intermediate/TurtleChallenge/main.py
@@ -4,20 +4,11 @@ lilly = Turtle()
 lilly.shape("turtle")
 lilly.color("Green")
 
-austin = Turtle()
-austin.shape("turtle")
-austin.color("Red")
-
-
-for i in range(4):
-    lilly.forward(100)
-    lilly.right(90)
-
-for i in range(4):
-    austin.forward(100)
-    austin.right(90)
-
-
+for i in range(50):
+    lilly.forward(2)
+    lilly.pu()
+    lilly.forward(2)
+    lilly.pd()
 
 
 
