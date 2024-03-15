712c4516ae2a80a87a4d2c6a6cd8ead6756be192
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-14 19:14:51 -0500

    Started Turtle Project

---
+++ Intermediate/TurtleChallenge/main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+lilly_the_turtle = Turtle()
+lilly_the_turtle.shape("classic")
+lilly_the_turtle.color("Green")
+lilly_the_turtle.forward(100)
+lilly_the_turtle.right(90)
+lilly_the_turtle.forward(100)
+lilly_the_turtle.right(90)
+lilly_the_turtle.forward(100)
+lilly_the_turtle.right(90)
+lilly_the_turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
