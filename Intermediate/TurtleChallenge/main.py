fa82078404a614c1be9cd3fdffa95c3e7ba5fc8b
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-16 18:44:04 -0500

    Created a random_walk

--- Intermediate/TurtleChallenge/main.py
+++ Intermediate/TurtleChallenge/main.py
@@ -3,19 +3,31 @@ import random
 
 lilly = Turtle()
 lilly.shape("turtle")
+lilly.pensize(10)
+lilly.speed(0)
 
-color = ["green", "orange", "yellow", "blue", "pink", "purple", "black", "dark green", "gray", "red", "dark green", "crimson", "medium violet red", "green yellow", "cornflower blue", "orange red"]
+color = ["green", "orange", "yellow", "blue", "pink", "purple", "black", "dark green", "gray", "red", "dark green", "crimson", "green yellow", "cornflower blue", "orange red"]
 
-def draw_shape(numb_sides):
-    lilly.color(random.choice(color))
-    angle = 360 / numb_sides
-    for _ in range(numb_sides):
-        lilly.forward(5)
-        lilly.right(angle)
+# def draw_shape(numb_sides):
+#     lilly.color(random.choice(color))
+#     angle = 360 / numb_sides
+#     for _ in range(numb_sides):
+#         lilly.forward(5)
+#         lilly.right(angle)
 
-for i in range(359, 360):
-    draw_shape(i)
+# for i in range(3, 10):
+#     draw_shape(i)
 
+direction = [0, 90, 180, 270]
+
+def random_walk(distance):
+    for i in range(distance):
+        lilly.color(random.choice(color))
+        lilly.forward(30)
+        lilly.setheading(random.choice(direction))
+
+
+random_walk(100)
 
 screen = Screen()
 screen.exitonclick()
