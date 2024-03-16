83c60ef1e65ba050ce15c64ac3ee93262479bcde
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-16 17:48:25 -0500

    made the perfect circle

--- Intermediate/TurtleChallenge/main.py
+++ Intermediate/TurtleChallenge/main.py
@@ -3,18 +3,17 @@ import random
 
 lilly = Turtle()
 lilly.shape("turtle")
-lilly.color("Green")
 
-color = ["green", "orange", "yellow", "blue", "pink", "purple", "black", "dark green", "gray", "red"]
+color = ["green", "orange", "yellow", "blue", "pink", "purple", "black", "dark green", "gray", "red", "dark green", "crimson", "medium violet red", "green yellow", "cornflower blue", "orange red"]
 
 def draw_shape(numb_sides):
     lilly.color(random.choice(color))
     angle = 360 / numb_sides
     for _ in range(numb_sides):
-        lilly.forward(100)
+        lilly.forward(5)
         lilly.right(angle)
 
-for i in range(3, 10):
+for i in range(359, 360):
     draw_shape(i)
 
 
