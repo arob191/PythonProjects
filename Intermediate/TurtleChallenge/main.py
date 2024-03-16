8485db38f27d2af8a0827ef6ac4f42027e6b7d94
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-16 17:39:32 -0500

    created triangle - decagon shapes

--- Intermediate/TurtleChallenge/main.py
+++ Intermediate/TurtleChallenge/main.py
@@ -1,16 +1,21 @@
 from turtle import Turtle, Screen
+import random
 
 lilly = Turtle()
 lilly.shape("turtle")
 lilly.color("Green")
 
-for i in range(50):
-    lilly.forward(2)
-    lilly.pu()
-    lilly.forward(2)
-    lilly.pd()
+color = ["green", "orange", "yellow", "blue", "pink", "purple", "black", "dark green", "gray", "red"]
 
+def draw_shape(numb_sides):
+    lilly.color(random.choice(color))
+    angle = 360 / numb_sides
+    for _ in range(numb_sides):
+        lilly.forward(100)
+        lilly.right(angle)
 
+for i in range(3, 10):
+    draw_shape(i)
 
 
 screen = Screen()
