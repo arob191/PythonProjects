a7e3983fe043f644f3f57b23457ef40d2e731df4
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-18 20:51:13 -0500

    code exercise 3. Using multiple methods to created a circle visual

--- Intermediate/TurtleChallenge/main.py
+++ Intermediate/TurtleChallenge/main.py
@@ -2,22 +2,13 @@ import turtle as t
 import random
 
 lilly = t.Turtle()
-lilly.shape("turtle")
-lilly.pensize(10)
+lilly.shape("classic")
+lilly.pensize(1)
 lilly.speed(0)
 t.colormode(255)
-
-# def draw_shape(numb_sides):
-#     lilly.color(random_color())
-#     angle = 360 / numb_sides
-#     for _ in range(numb_sides):
-#         lilly.forward(5)
-#         lilly.right(angle)
-
-# for i in range(3, 10):
-#     draw_shape(i)
-
 direction = [0, 90, 180, 270]
+heading = 0
+num_of_circles = 50
 
 def random_color():
     r = random.randint(0, 255)
@@ -25,14 +16,34 @@ def random_color():
     b = random.randint(0, 255)
     return (r, g, b)
 
+# Code exercise 1
+def draw_shape(numb_sides):
+    lilly.color(random_color())
+    angle = 360 / numb_sides
+    for _ in range(numb_sides):
+        lilly.forward(5)
+        lilly.right(angle)
+# for i in range(3, 10):
+#     draw_shape(i)
+
+# Code exercise 2
 def random_walk(distance):
     for i in range(distance):
         lilly.color(random_color())
         lilly.forward(30)
         lilly.setheading(random.choice(direction))
+# random_walk(100)
 
 
-random_walk(100)
+# Code exercise 3
+def repeating_circles(num_of_circles):
+    angle = 360 / num_of_circles
+    for _ in range(num_of_circles +1):
+        lilly.color(random_color())
+        lilly.circle(100)
+        lilly.seth(lilly.heading() + angle)
+
+repeating_circles(heading, num_of_circles)
 
 screen = t.Screen()
 screen.exitonclick()
