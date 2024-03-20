d54a2a48eea38aac7dd92f8df375e025a291abbd
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-20 18:52:38 -0500

    No more need for extracting colors from the image each time. Copied all tuples to make a static list

--- Intermediate/HirstPaintingProject/main.py
+++ Intermediate/HirstPaintingProject/main.py
@@ -2,22 +2,23 @@ import colorgram
 import turtle
 import random
 
-colors = colorgram.extract("dhirst.jpg", 26)
-rgbs = []
+# colors = colorgram.extract("dhirst.jpg", 27)
+rgbs = [(199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 224, 227), (186, 158, 53), (6, 57, 83), (109, 67, 85), (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47), (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186), (172, 153, 159), (212, 183, 177), (176, 198, 203), (150, 115, 120), (202, 185, 190)]
 painter = turtle.Turtle()
 turtle.colormode(255)
 painter.penup()
 painter.setx(-150)
 painter.sety(150)
 
-for rgb in colors:
-    first_rgb = rgb.rgb
-    r = int(first_rgb.r)
-    g = int(first_rgb.g)
-    b = int(first_rgb.b)
-    rgb_tuple = (r, g, b)
-    rgbs.append(rgb_tuple)
+# for rgb in colors:
+#     first_rgb = rgb.rgb
+#     r = int(first_rgb.r)
+#     g = int(first_rgb.g)
+#     b = int(first_rgb.b)
+#     rgb_tuple = (r, g, b)
+#     rgbs.append(rgb_tuple)
 
+# print(rgbs)
 
 painter.color(random.choice(rgbs))
 painter.dot(50)
