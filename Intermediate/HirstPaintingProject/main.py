c5163b62008592271d504a425a5468c12265545b
Austin Robertson <austinwrobertson98@gmail.com>
2024-03-20 19:20:02 -0500

    Hid the turtle from the final results

--- Intermediate/HirstPaintingProject/main.py
+++ Intermediate/HirstPaintingProject/main.py
@@ -7,8 +7,11 @@ rgbs = [(199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 2
 painter = turtle.Turtle()
 turtle.colormode(255)
 painter.penup()
-painter.setx(-150)
-painter.sety(150)
+painter.hideturtle()
+painter.speed(0)
+painter.setx(-250)
+painter.sety(-250)
+
 
 # for rgb in colors:
 #     first_rgb = rgb.rgb
@@ -17,11 +20,22 @@ painter.sety(150)
 #     b = int(first_rgb.b)
 #     rgb_tuple = (r, g, b)
 #     rgbs.append(rgb_tuple)
-
 # print(rgbs)
 
-painter.color(random.choice(rgbs))
-painter.dot(50)
+
+def make_row(times):
+    for _ in range(times):
+        for _ in range(10):
+            painter.dot(10, random.choice(rgbs))
+            painter.forward(50)
+        painter.setheading(90)
+        painter.forward(50)
+        painter.setheading(180)
+        painter.forward(500)
+        painter.setheading(0)
+
+
+make_row(10)
 
 
 screen = turtle.Screen()
