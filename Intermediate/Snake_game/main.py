c2dfeb586a60ba4b1fb7f9b077650d283f745bc6
arob191 <austinwrobertson98@gmail.com>
2024-03-23 14:10:24 -0500

    Set the segments to follow the one infront of it

--- Intermediate/Snake_game/main.py
+++ Intermediate/Snake_game/main.py
@@ -1,11 +1,14 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 turtles = []
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+game_is_on = True
 
 for snakes in range(0, 3):
     snake = Turtle(shape="square", )
@@ -14,9 +17,16 @@ for snakes in range(0, 3):
     snake.setposition(starting_positions[snakes])
     turtles.append(snake)
 
+screen.update()
 
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-
+    for snake in range(len(turtles) - 1, 0, -1):
+        new_x = turtles[snake - 1].xcor()
+        new_y = turtles[snake - 1].ycor()
+        turtles[snake].goto(new_x, new_y)
 
 
 
