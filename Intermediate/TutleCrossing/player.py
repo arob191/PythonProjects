4964cbe64c092d3f7e1d21e74923e0b10d683ab4
Austin Robertson <austinwrobertson98@gmail.com>
2024-04-27 20:42:51 -0500

    Started on player class

--- Intermediate/TutleCrossing/player.py
+++ Intermediate/TutleCrossing/player.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self, shape = "turtle"):
+        super().__init__(shape)
+        self.penup()
+        self.color("red")
+        self.setheading(90)
+        self.set()
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def set(self):
+        self.goto(STARTING_POSITION)
+
